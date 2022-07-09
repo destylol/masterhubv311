836a58d03687516c3a680ed74d916fe3121bb719
desty <83312843+destylol@users.noreply.github.com>
2022-07-09 11:22:01 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,25 +1,30 @@
 local Games = {
-   1224212277,
-   520749081,
-   537413528,
-		6284583030,
-		9049840490,
+    1224212277,
+    520749081,
+    537413528,
+    6284583030,
+    9049840490,
     13822889,
-		7560156054,
-		8750997647,
-		8884433153,
-		3823781113,
-		3587619225,
-		9555732501,
+    7560156054,
+    8750997647,
+    8884433153,
+    3823781113,
+    3587619225,
+    9555732501,
     9551640993
-	}
+}
 
 if not table.find(Games, game.PlaceId) then
-  game:GetService("StarterGui"):SetCore("SendNotification", {
-				Title = "Master Hub",
-				Text = "Game Is Not Supported!",
-				Duration = 5
-			})
-  else
-  		loadstring(game:HttpGet("https://raw.githubusercontent.com/destylol/masterhubv311/itachi/" .. game.PlaceId .. ".lua"))()
-  end
+    game:GetService("StarterGui"):SetCore(
+        "SendNotification",
+        {
+            Title = "Master Hub",
+            Text = "Game Is Not Supported!",
+            Duration = 5
+        }
+    )
+else
+    loadstring(
+        game:HttpGet("https://raw.githubusercontent.com/destylol/masterhubv311/itachi/" .. game.PlaceId .. ".lua")
+    )()
+end

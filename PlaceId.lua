d824ac8e5bee1a3892d521bbe055a3546a94bd27
GramxPersons <97328690+GramxPerson@users.noreply.github.com>
2022-09-18 07:43:43 +0700

    Create PlaceId.lua

---
+++ PlaceId.lua
@@ -0,0 +1,60 @@
+local GameSupport = {
+    [6284583030] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [520749081] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [537413528] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [9049840490] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [13822889] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [7560156054] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [8750997647] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [8884433153] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [3823781113] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [3587619225] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [9555732501] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [9551640993] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [6299805723] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+    [10321372166] = {
+        Data = {Maintenance = false},
+        MultiPlaceId = nil
+    },
+}
+
+return GameSupport

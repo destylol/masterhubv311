39d22745d823b1d7bd8d4766ebae36095c17ecbb
GramxPersons <97328690+GramxPerson@users.noreply.github.com>
2022-09-20 19:29:23 +0700

    Update PlaceId.lua

--- PlaceId.lua
+++ PlaceId.lua
@@ -1,59 +1,48 @@
 local GameSupport = {
     [6284583030] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {10321372166}
+    },
+    [10321372166] = {
+        Data = {Maintenance = false},
     },
     [520749081] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [537413528] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [9049840490] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [13822889] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [7560156054] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [8750997647] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [8884433153] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [3823781113] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [3587619225] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [9555732501] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [9551640993] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [6299805723] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
     [6152116144] = {
         Data = {Maintenance = false},
-        MultiPlaceId = {0}
     },
 }
 
